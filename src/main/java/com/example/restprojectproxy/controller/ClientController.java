dd1e219eb54dbb4711a2a3bae9a7ba57a292aecf
Timur <timkonovalov9858@gmail.com>
2023-05-09 11:38:07 +0300

    sl4j logger init into controller

--- src/main/java/com/example/restprojectproxy/controller/ClientController.java
+++ src/main/java/com/example/restprojectproxy/controller/ClientController.java
@@ -5,10 +5,12 @@ import com.example.restprojectproxy.service.ClientService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @Tag(name = "Clients")
 @RequiredArgsConstructor
@@ -20,23 +22,27 @@ public class ClientController {
     @PostMapping(value = "/clients")
     public void createClient(@RequestBody ClientDTO clientDTO) {
         clientService.createNewClient(clientDTO);
+        log.info("client: " + clientDTO.toString() + " add");
     }
 
     @Operation(summary = "Вывести всех клиентов", tags = {"Clients"})
     @GetMapping(value = "/clients")
     public List<ClientDTO> showAllClients() {
+        log.info("display all clients");
         return clientService.displayAllClients();
     }
 
     @Operation(summary = "Вывести клиента по id", tags = {"Clients"})
     @GetMapping(value = "/clients/{id}")
     public ClientDTO getClientById(@PathVariable(name = "id") Integer id) {
+        log.info("return from db client: " + clientService.getOneClientById(id).toString());
         return clientService.getOneClientById(id);
     }
 
     @Operation(summary = "Вывести клиента по name", tags = {"Clients"})
     @GetMapping(value = "/clients/name/{name}")
     public ClientDTO getClientByName(@PathVariable(name = "name") String name) {
+        log.info("return from db client: " + clientService.getOneClientByName(name).toString());
         return clientService.getOneClientByName(name);
     }
 
@@ -44,11 +50,13 @@ public class ClientController {
     @PutMapping(value = "/clients/{id}")
     public void сhangeClientData(@PathVariable(name = "id") Integer id, @RequestBody ClientDTO clientDTO) {
         clientService.сhangeClientDataById(id, clientDTO);
+        log.info("client data by id: " + id + " changed to: " + clientDTO.toString());
     }
 
     @Operation(summary = "Удалить клиента по id", tags = {"Clients"})
     @DeleteMapping(value = "/clients/{id}")
     public void deleteClientById(@PathVariable(name = "id") Integer id) {
         clientService.deleteOneClientById(id);
+        log.info("remove client by id: " + id);
     }
 }
